088bb58af86a881b25eaf8de874ffb686481184e
Saleh-Binessa <saleh.binessa@gmail.com>
2024-04-16 15:46:56 +0300

    merge two task in one file

---
+++ BankCMS/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using BankCMS_.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankCMS_.Controllers
+{
+    public class EmployeeController : Controller
+    {
+           private List<Employee> employees = new List<Employee>
+    {
+        new Employee { EmployeeId = 1, FirstName = "Saleh", LastName = "Bin Essa", Department = "IT", Email = "saleh.binessa@gmail.com"},
+        new Employee { EmployeeId = 2, FirstName = "Nawaf", LastName = "Almutairi", Department = "IT", Email = "nawaf.almutairi@gmail.com"},
+    };
+        public IActionResult Index()
+        {
+            return View(employees);
+        }
+
+        public IActionResult EmployeeDetails(int id)
+        {
+            var employee = employees.FirstOrDefault(e => e.EmployeeId == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+    }
+}
